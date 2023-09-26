de36af20481c0743d46c38a341da8fa86513ec5a
atharv0811 <karnekaratharv12@gmail.com>
2023-09-26 13:40:25 +0530

    created routes to clean up the code and added 404 page not found

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product', (req, res, next) => {
-    res.send("<form action='/product' method='POST'><input placeholder='Product Name' type='text' name='title'><input placeholder='size' type='number' name='size'><button type='submit'>Add Product</button></form>")
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page Not Found</h1>")
 })
 
-app.use('/', (req, res, next) => {
-    res.send("<h1>hello</h1>")
-    // res.send({ key1: "value" })
-});
-
-const server = http.createServer(app);
-
-server.listen(4000);
+app.listen(4000);
---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.send("<h1>hello</h1>")
+    // res.send({ key1: "value" })
+});
+
+module.exports = router;
