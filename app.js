aa3b0f69a012dd63f90e1f78556c88612c9ef967
atharv0811 <karnekaratharv12@gmail.com>
2023-09-26 13:09:18 +0530

    installed body-parser and parsed the data and log into the console

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("in the middleware");
-    next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send("<form action='/product' method='POST'><input placeholder='Product Name' type='text' name='title'><input placeholder='size' type='number' name='size'><button type='submit'>Add Product</button></form>")
 });
 
-app.use((req, res, next) => {
-    console.log("in the another middleware");
-    // res.send("<h1>hello</h1>")
-    res.send({ key1: "value" })
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    res.send("<h1>hello</h1>")
+    // res.send({ key1: "value" })
 });
 
 const server = http.createServer(app);
